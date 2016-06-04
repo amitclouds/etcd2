11c65eed52e99267533b54066cf53b73f716e197
amitclouds <amitclouds@gmail.com>
2016-06-04 17:34:20 +0530

    Update _load_key.rb

--- ncloud-etcd/recipes/_load_key.rb
+++ ncloud-etcd/recipes/_load_key.rb
@@ -1,9 +1,9 @@
 
-potentially_at_compile_time do
+
   remote_file '/tmp/key' do
     source 'http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem'
     owner 'root'
     group  'root'
     mode '0644'
   end
-end
+
