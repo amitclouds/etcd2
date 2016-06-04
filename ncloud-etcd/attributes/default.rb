94a7731510806fb59a943559d7f60daac0d41c51
amitclouds <amitclouds@gmail.com>
2016-06-04 17:00:53 +0530

    etcd

---
+++ ncloud-etcd/attributes/default.rb
@@ -0,0 +1,17 @@
+
+default['ncloud-etcd']['service_name'] = 'ncloud'
+default['confd']['service_name'] = node['ncloud-etcd']['service_name']
+default['confd']['config'] = {
+  'backend' => 'etcd',
+  'confdir' => '/etc/confd',
+  'interval' => 600,
+  'nodes' => 'http://127.0.0.1:2379',
+  'noop' => false,
+  'scheme' => 'http'
+}
+
+default['ncloud-confd']['prefix'] = '/'
+default['ncloud-confd']['keys'] = '/foo'
+
+
+default['build-essential']['compile_time'] = true
---
+++ ncloud-etcd/recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: ncloud-etcd
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'ncloud-etcd::_load_key'
+etcd_installation_binary node['ncloud-etcd']['service_name'] do
+  action :create
+end
+
+etcd_service_manager_upstart node['ncloud-etcd']['service_name'] do
+  action :start
+end
+
+include_recipe 'confd'
+
+service = node['ncloud-etcd']['service_name']
+
+#val = File.read('/tmp/key')
+#etcd_key "/foo" do
+#  value val
+#  action 'set'
+  #port 4001
+#  port 2379
+#end
+
+confd_template '/tmp/foo' do
+  template_source 'foo.tmpl.erb'
+  prefix node['ncloud-confd']['prefix']
+  keys node['ncloud-confd']['keys']
+
+  notifies :restart, "confd_service[#{service}]", :delayed
+end
